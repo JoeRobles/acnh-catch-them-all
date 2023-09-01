9a37a8a83ef702ba025faa2a7cce2b76a319f9d9
Joe Robles <joe.robles@acklenavenue.com>
2023-09-01 13:57:19 -0500

    language in song detail implemented

--- src/app/components/song-detail/song-detail.component.ts
+++ src/app/components/song-detail/song-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { SongModel } from '../../acnhapi/song/models/song.model';
 import { CritterService } from '../../shared/services/critter.service';
+import { LanguageTypeEnum } from '../../shared/models/language-type.enum';
 
 @Component({
   selector: 'app-song-detail',
@@ -12,9 +13,11 @@ export class SongDetailComponent implements OnInit {
   formModal: any;
   song: SongModel = {} as SongModel;
   songMusicUri = '';
+  language: LanguageTypeEnum = LanguageTypeEnum.NameUSen;
   constructor(private critterService: CritterService) {
   }
   ngOnInit() {
     this.critterService.song.subscribe(s => this.song = s);
+    this.critterService.language$.subscribe(l => this.language = l);
   }
 }

736e8e0fe194cf560d8b00f54fd0c7fd0f0fef1b
Joe Robles <joe.robles.pdj@gmail.com>
2024-05-11 11:45:21 -0500

    api calls implemented

--- src/app/shared/services/critter-api.service.ts
+++ src/app/shared/services/critter-api.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 
@@ -26,67 +26,73 @@ import { environment } from '../../../environments/environment';
 })
 export class CritterApiService {
 
+  options = {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    }
+  }
   apiUrl = environment.apiUrl + '/nh/v1';
+
   constructor(private http: HttpClient) { }
 
   getBugs(): Observable<BugModel[]> {
-    return this.http.get<BugResponseInterface[]>(`${this.apiUrl}/bugs`)
+    return this.http.get<BugResponseInterface[]>(`${this.apiUrl}/bugs`, this.options)
       .pipe(
         map(bugs => bugs.map(bug => new BugModel(bug)))
       );
   }
 
   getBugModels(): Observable<BugModelModel[]> {
-    return this.http.get<ModelResponseInterface[]>(`${this.apiUrl}/bug-models`)
+    return this.http.get<ModelResponseInterface[]>(`${this.apiUrl}/bug-models`, this.options)
       .pipe(
         map(bugModels => bugModels.map(bugModel => new BugModelModel(bugModel)))
       );
   }
 
   getFish(): Observable<FishModel[]> {
-    return this.http.get<FishResponseInterface[]>(`${this.apiUrl}/fish`)
+    return this.http.get<FishResponseInterface[]>(`${this.apiUrl}/fish`, this.options)
       .pipe(
         map(fish => fish.map(f => new FishModel(f)))
       );
   }
 
   getFishModels(): Observable<FishModelModel[]> {
-    return this.http.get<ModelResponseInterface[]>(`${this.apiUrl}/fish-models`)
+    return this.http.get<ModelResponseInterface[]>(`${this.apiUrl}/fish-models`, this.options)
       .pipe(
         map(fishModels => fishModels.map(fishModel => new FishModelModel(fishModel)))
       );
   }
 
   getSea(): Observable<SeaModel[]> {
-    return this.http.get<SeaResponseInterface[]>(`${this.apiUrl}/sea`)
+    return this.http.get<SeaResponseInterface[]>(`${this.apiUrl}/sea`, this.options)
       .pipe(
         map(sea => sea.map(s => new SeaModel(s)))
       );
   }
 
   getSongs(): Observable<SongModel[]> {
-    return this.http.get<SongResponseInterface[]>(`${this.apiUrl}/songs`)
+    return this.http.get<SongResponseInterface[]>(`${this.apiUrl}/songs`, this.options)
       .pipe(
         map(songs => songs.map(s => new SongModel(s)))
       );
   }
 
   getFossils(): Observable<FossilModel[]> {
-    return this.http.get<FossilResponseInterface[]>(`${this.apiUrl}/fossils`)
+    return this.http.get<FossilResponseInterface[]>(`${this.apiUrl}/fossils`, this.options)
       .pipe(
         map(fossil => fossil.map(f => new FossilModel(f)))
       );
   }
 
   getArt(): Observable<ArtModel[]> {
-    return this.http.get<ArtResponseInterface[]>(`${this.apiUrl}/art`)
+    return this.http.get<ArtResponseInterface[]>(`${this.apiUrl}/art`, this.options)
       .pipe(
         map(art => art.map(a => new ArtModel(a)))
       );
   }
 
   getMusic(): Observable<MusicModel[]> {
-    return this.http.get<MusicResponseInterface[]>(`${this.apiUrl}/hourly`)
+    return this.http.get<MusicResponseInterface[]>(`${this.apiUrl}/hourly`, this.options)
       .pipe(
         map(music => music.map(m => new MusicModel(m)))
       );
